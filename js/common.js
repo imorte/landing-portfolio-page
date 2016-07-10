d48abf35c80844fd04b9a157313835c4b1134671
Kirill <Kirill@Mac-mini.local>
2016-07-10 14:51:59 +0700

    header

--- js/common.js
+++ js/common.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
-	$(".main_head").css('min-height', $(document).height());
-});
+
+	function heightDetect(){
+		$(".main_head").css('height', $(window).height());
+	}
+
+	heightDetect();
+
+	$(window).resize(function(){
+		heightDetect();
+	});
+
+	$(window).load(function(){
+		$('.loaderInner').fadeOut();
+		$('.loader').delay(400).fadeOut('slow');
+	})
+
+	$('.sandwich').on('click', function(){
+		$('.sandwich').toggleClass('active');
+	})
+
+	$('.toggle_mnu').on('click', function(){
+		if($(".top_mnu").is(":visible")){
+			$('.top_mnu').fadeOut(600);
+		}else{
+			$('.top_mnu').fadeIn(600);
+		}
+	})
+	
+}); 
